3727947ef59813a02db3a5966c30f440e8dc28dc
scabezas632 <samuel.cabezas.632@gmail.com>
2018-08-23 20:08:37 -0300

    Server corriendo con express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 
-console.log('Código de TypeScript');
+import Server from './server/server';
+
+const server = Server.init( 3000 );
+
+server.start( ()=>{
+    console.log('Servidro corriendo en el puerto 3000');
+});
+
